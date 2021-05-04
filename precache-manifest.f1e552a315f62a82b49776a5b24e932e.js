f357bf27dc2a5517b3ba4e8a1a6b87bf4a352d45
alexaergun <alexaergun@gmail.com>
2021-05-04 16:22:33 -0600

    Updates

---
+++ precache-manifest.f1e552a315f62a82b49776a5b24e932e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "498d2b6139eee00e22a48c6360a72dbf",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "0eb210c73e6bf46528df",
+    "url": "/my-portfolio/static/css/2.2d0dd394.chunk.css"
+  },
+  {
+    "revision": "de3bb9eee9d0949dd8bb",
+    "url": "/my-portfolio/static/css/main.cf61d1fb.chunk.css"
+  },
+  {
+    "revision": "0eb210c73e6bf46528df",
+    "url": "/my-portfolio/static/js/2.401e7336.chunk.js"
+  },
+  {
+    "revision": "de3bb9eee9d0949dd8bb",
+    "url": "/my-portfolio/static/js/main.d19ed7f4.chunk.js"
+  },
+  {
+    "revision": "2b237f782697ac6a05db",
+    "url": "/my-portfolio/static/js/runtime-main.e961d79d.js"
+  },
+  {
+    "revision": "1a6386f927278bed74c87c9841452aad",
+    "url": "/my-portfolio/static/media/DayPlanner1.1a6386f9.png"
+  },
+  {
+    "revision": "2b4c617a95d2d5bb00e980a76dc0ecbc",
+    "url": "/my-portfolio/static/media/GameBuzz.2b4c617a.png"
+  },
+  {
+    "revision": "11e956222ede1fedf8a63875b89cc825",
+    "url": "/my-portfolio/static/media/HobbyDrop.11e95622.png"
+  },
+  {
+    "revision": "b9cf8cb77722189d19a0ca8e3612f93e",
+    "url": "/my-portfolio/static/media/TopBoardGames.b9cf8cb7.png"
+  },
+  {
+    "revision": "7cfbed606639a4e7e74fe9710728a862",
+    "url": "/my-portfolio/static/media/WeatherDash1.7cfbed60.png"
+  },
+  {
+    "revision": "1b2da0f2fdde0add9e08cb96e6d05559",
+    "url": "/my-portfolio/static/media/cloud-backend.1b2da0f2.svg"
+  },
+  {
+    "revision": "a49fe13b8f9fedb0d94a8ca9387f030f",
+    "url": "/my-portfolio/static/media/cloud-databases.a49fe13b.svg"
+  },
+  {
+    "revision": "9105a6574c2b90ac9a19406b9fe2ab51",
+    "url": "/my-portfolio/static/media/cloud-frontend.9105a657.svg"
+  },
+  {
+    "revision": "3290a95597577f67b994a290a10fcb63",
+    "url": "/my-portfolio/static/media/cloud-services.3290a955.svg"
+  },
+  {
+    "revision": "7447a07fb9a2554076b477be54b45c4c",
+    "url": "/my-portfolio/static/media/github.7447a07f.svg"
+  },
+  {
+    "revision": "feb2f8ec3e6dfc08c6bf4a7a2ab9f497",
+    "url": "/my-portfolio/static/media/hummingbird.feb2f8ec.svg"
+  },
+  {
+    "revision": "55be353564668d29eb5469ea6cb7506d",
+    "url": "/my-portfolio/static/media/linkedin.55be3535.svg"
+  },
+  {
+    "revision": "455a8406680358656e75212c83c5a716",
+    "url": "/my-portfolio/static/media/resume.455a8406.svg"
+  },
+  {
+    "revision": "783e7fd97e70ea74774a0be1af03798b",
+    "url": "/my-portfolio/static/media/sun.783e7fd9.svg"
+  }
+]);
